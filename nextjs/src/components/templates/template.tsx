ea33a755d1d26ab5985530c7319ecdd00a15d25c
Tom147-github <tom147.github@gmail.com>
2024-01-26 16:59:35 +0900

    新規登録ボタン・ログインボタンを配置

--- nextjs/src/components/templates/template.tsx
+++ nextjs/src/components/templates/template.tsx
@@ -1,1 +1,19 @@
 "use client";
+
+import { ReactElement } from "react";
+import FormButton from "@/components/atoms/buttons/formButton";
+
+const Template = (): ReactElement => {
+  return (
+    <>
+      <div>
+        <FormButton text={"Log in"} />
+      </div>
+      <div>
+        <FormButton text={"Sign up"} />
+      </div>
+    </>
+  );
+};
+
+export default Template;
